47d0e0ce470e8066e88169de464b716b67caecff
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-11 14:52:18 +0530

    for loop function added

---
+++ for_loop.js
@@ -0,0 +1,46 @@
+// Loops in js
+/*
+5 loops
+1. For
+2. For in
+3. For Each
+4. While
+4. Do While
+*/
+
+/* For loop example start */
+console.log(`For loop 1 start`);
+
+for (i = 0; i < 10; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
+console.log(`For loop 1 end`);
+console.log(`\n`);
+console.log(`For loop 2 start`);
+
+for (i = 0; i < 10; i++) {
+  task(i);
+}
+console.log(`For loop 2 end`);
+console.log(`\n`);
+console.log(`For loop 3 start`);
+
+for (let i = 0; i < 10; i++) {
+  task(i);
+}
+console.log(`For loop 3 end`);
+console.log(`\n`);
+function task(i) {
+  /*
+  setTimeout(function () {
+    console.log(i);
+  }, 2000 * i);
+
+  */
+  setTimeout(function () {
+    console.log(i);
+  }, 500);
+}
+/* For loop example en */
