0df2112dfdee00336e354d7602a7e9b383b93f66
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-11 18:36:20 +0530

    do while function added

--- while_loop.js
+++ while_loop.js
@@ -1,7 +1,21 @@
 /* While loop start */
+console.log(`While loop start`);
 let k = 3;
 while (k <= 20) {
   console.log(`K= ${k}`);
   k++;
 }
+console.log(`While loop end`);
 /* While loop end */
+console.log(`\n`);
+
+/* Do While loop start */
+console.log(`DO While loop start`);
+let q = 3;
+do {
+  console.log(`Q= ${q}`);
+  q++;
+} while (q <= 20);
+console.log(`DO While loop end`);
+/* Do While loop end */
+console.log(`\n`);
