5cf4629ef32746791b36892af3cc7ab0565a6a14
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-14 07:29:13 +0530

    string method files added

---
+++ string_methods.js
@@ -0,0 +1,15 @@
+console.log('sfdhgsdjf');
+/*
+1. charAt()
+2. charCodeAt()
+3. startsWith()
+4. endsWith()
+5. from charCodeAt()
+6. includes()
+7. indexOf()
+8. lastIndexOf()
+9. match()
+10. repeat()
+11. replace()
+12. 
+*/
