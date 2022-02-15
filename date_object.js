2843d8eb94138dcd12f300ace52f0ce0e02fee5b
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-15 07:21:08 +0530

    date objects update

--- date_object.js
+++ date_object.js
@@ -4,4 +4,21 @@
 3. toString();
 4. toDateString();
 5. toUTCString() // Returns Universal Time Coordinated (UTC) time
+6. getTime()
+7. getFullYear()
+8. getMonth() // Starts with 0
+9. getDay()
+10. getDate()
+11. getHours()
+12. getMinutes()
+13. getSeconds()
+14. getMilliSeconds()
+15. setTime()
+16. setFullYear()
+17. setMonth()
+18. setDate()
+19. setHours()
+20. setMinutes()
+21. setSconds()
+22. setMilliSeconds()
 */
