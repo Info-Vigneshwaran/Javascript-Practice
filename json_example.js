d00cc390c7c231b65442a5f1c0a991d3f97e5d94
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-16 15:24:22 +0530

    json example file added

---
+++ json_example.js
@@ -0,0 +1,12 @@
+/*
+1. javascript object notation
+2. lightweight data-interchange format
+3. JSON is a subset of javascript
+4. It is used with AJAX
+5. It is very easy to read / write
+
+//
+1. JSON.stringify() // convert javascript  object to JSON object
+2. JSON.pars() // convert JSON object to javascript object
+//
+*/
