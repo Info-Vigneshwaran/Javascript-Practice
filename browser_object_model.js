41df0a0db43b7afcd81ac69bf2c3824a4b5ce4ff
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-16 07:34:51 +0530

    browser ojbect properties added

--- browser_object_model.js
+++ browser_object_model.js
@@ -1,2 +1,19 @@
 /*
+
+// Window Properties
+1. window.innerHeight // inner element's browser height
+2. window.innerWidth // inner element's browser width
+3. window.outerHeight // entire browser's height
+4. window.outerWidth // entire browser's width
+5. window.open
+6. window.moveTo
+7. window.focus
+8. window.close
+// Window Properties
+
+// Screen Properties
+1. screen.height
+1. screen.avialHeight
+1. screen.width
+// Screen Properties
 */
