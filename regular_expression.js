34c580f3fc1e8c1a40912bde14f140111593b08b
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-02-17 18:13:25 +0530

    brackets method added in regular expression

---
+++ regular_expression.js
@@ -0,0 +1,24 @@
+/*
+
+1. /a/g // g-global search
+1. /a/i // i-case insensitive search
+1. /a/ig
+1. /a/igm // m-search with multiple lines
+1. new RegExp("a","g")
+
+/* Regular expression methods */
+/*
+1. match // 
+2. search // returns index value
+*/
+/* Regular expression methods */
+
+/* Brackets start
+1. /[a]/g
+1. /[^a]/g
+1. /[^a]/ig
+1. /[^a]/igm
+Brackets end */
+
+/* Metacharacters start
+Metacharacters end */
