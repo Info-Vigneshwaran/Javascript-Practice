96843e79f78fca1d844f29461913034930883990
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-09 21:54:07 +0530

    1 file modified

--- practise/5.js
+++ practise/5.js
@@ -17,3 +17,34 @@ let calc = calculation.add(2).multi(3).multi(8);
 console.log(calc.total);
 
 //https://github.com/Ebazhanov/linkedin-skill-assessments-quizzes/blob/master/javascript/javascript-quiz.md
+
+var value = 2568,
+  sum = value
+    .toString()
+    .split('')
+    .map(Number)
+    .reduceRight(function (a, b) {
+      return a + b;
+    }, 0);
+
+console.log(sum);
+
+var value = 2568,
+  sum = 0;
+
+while (value) {
+  sum += value % 10;
+  value = Math.floor(value / 10);
+}
+
+console.log(sum);
+
+function a() {
+  let a = 'ABC';
+  return function () {
+    let b = 'def';
+    console.log(a, b);
+  };
+}
+let x = a();
+x();
