a983a418eb7e13b5d4aa98e3401eedd363036314
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-09 19:16:50 +0530

    1 file added

---
+++ practise/5.js
@@ -0,0 +1,17 @@
+const calculation = {
+  total: 0,
+  add(a) {
+    this.total += a;
+    return this;
+  },
+  multi(a) {
+    this.total *= a;
+    return this;
+  },
+  sub(a) {
+    this.total -= a;
+    return this;
+  },
+};
+let calc = calculation.add(2).multi(3).multi(8);
+console.log(calc.total);
