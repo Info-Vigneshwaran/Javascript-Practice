ed134d3fb2e78e3481371ef20aa9bcee8ab5729d
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-09 08:19:17 +0530

    1 file modified

--- practise/4.js
+++ practise/4.js
@@ -13,3 +13,17 @@ let f = '8';
 let a = 1;
 console.log(+f + a + 1);
 console.log(f + a + 1);
+
+function* fun() {
+  yield 10;
+  yield 20;
+  yield 30;
+}
+
+// Calling the Generate Function
+var gen = fun();
+console.log(gen.next().value);
+console.log('<br>');
+console.log(gen.next().value);
+console.log('<br>');
+console.log(gen.next().value);
