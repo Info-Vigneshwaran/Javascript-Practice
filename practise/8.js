8d5b44e55c52986ccc2f959e6952a1bb39e7395f
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-11 15:49:02 +0530

    1 file modified

--- practise/8.js
+++ practise/8.js
@@ -1,12 +1,24 @@
-let str = 'test new gues';
-let convert = str.split(' ').map((str) => {
-  return str.charAt().toUpperCase();
-});
-console.log(convert);
+// let str = 'test new gues';
+// let convert = str.split(' ').map((str) => {
+//   return str.charAt().toUpperCase();
+// });
+// console.log(convert);
 
-const users = [
-  { name: 'John', id: 1, city: 'Delhi' },
-  { name: 'Max', id: 2, city: 'London' },
-  { name: 'Rod', id: 3, city: 'Paris' },
-];
-console.table(users);
+// const users = [
+//   { name: 'John', id: 1, city: 'Delhi' },
+//   { name: 'Max', id: 2, city: 'London' },
+//   { name: 'Rod', id: 3, city: 'Paris' },
+// ];
+// console.table(users);
+
+// console.log(0.1 + 0.2);
+// console.log(0.2 + 0.5);
+
+var myChars = { a: 'd', b: 'gj' };
+delete myChars.a;
+console.log(myChars);
+
+var myChars = [20, 30, 55, 10];
+delete myChars[0];
+console.log(myChars);
+console.log(myChars[0]);
