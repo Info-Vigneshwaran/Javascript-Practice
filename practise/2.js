f0931ed3c4e5536615c08e98487d769bcd7ac73b
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-12 13:50:14 +0530

    1 file modified

--- practise/2.js
+++ practise/2.js
@@ -1,17 +1,17 @@
 // a(5);
-var test = 'dfjhgsjhf';
-var a = (a, b) => {
-  console.log(test);
-  var test = 'dd33';
-  console.log(test);
-  console.log(a);
-};
-a(5);
+// var test = 'dfjhgsjhf';
+// var a = (a, b) => {
+//   console.log(test);
+//   var test = 'dd33';
+//   console.log(test);
+//   console.log(a);
+// };
+// a(5);
 
-console.log(test);
+// console.log(test);
 
-var x = 5;
-console.log(typeof typeof x);
+// var x = 5;
+// console.log(typeof typeof x);
 
 function mul(a) {
   return function (b) {
