570188f4b85fad15640c50a6381f1c216d54ae92
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-14 13:19:49 +0530

    1 file modified

--- practise/9.js
+++ practise/9.js
@@ -4,3 +4,19 @@ function mul(a) {
   };
 }
 console.log(mul(2)(4)(5)('add'));
+
+let arrayIntegers = [1, 2, 3, 4, 5];
+let arrayIntegers1 = arrayIntegers.slice(0, 2); // returns [1,2]
+let arrayIntegers2 = arrayIntegers.slice(2, 3); // returns [3]
+let arrayIntegers3 = arrayIntegers.slice(4); //returns [5]
+console.log(arrayIntegers);
+console.log(arrayIntegers3);
+
+var mainString = 'helloas 34',
+  subString = 'hell';
+console.log(mainString.includes(subString));
+console.log(mainString.match(/\d+/));
+
+var r = /\d+/;
+var s = 'you can enter maximum 500 choices';
+console.log(s.match(r));
