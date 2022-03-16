8914a297a975bb9b30fe3b19f63d794a643d10fc
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-16 10:12:11 +0530

    1 file modified

--- practise/9.js
+++ practise/9.js
@@ -15,7 +15,7 @@ console.log(arrayIntegers3);
 var mainString = 'helloas 34 56 88 54 11',
   subString = 'hell';
 console.log(mainString.includes(subString));
-console.log(mainString.match(/\d+/));
+console.log(mainString.match(/\d+/g));
 
 var r = /\d+/;
 var s = 'you can enter maximum 500 choices';
