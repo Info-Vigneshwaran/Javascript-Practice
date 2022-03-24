a0dce3eaaf1f5cdca1712f32154460b22641749f
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-24 17:53:58 +0530

    1 file added

---
+++ practise/14.js
@@ -0,0 +1,15 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 9];
+// console.log(array.reverse());
+// array.forEach((index, value) => {
+//   console.log(index);
+// });
+// let new_array = [];
+// for (i = array.length; i > 0; i--) {
+//   new_array.push(array[i - 1]);
+// }
+
+// let new_array = array.reduceRight((prevValue, current) => {
+//   prevValue.push(current);
+//   return prevValue;
+// }, []);
+// console.log(new_array);
