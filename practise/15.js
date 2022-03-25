1ba29c6e61e93660658a3bf4201ba342b2ffbd96
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-25 16:50:27 +0530

    1 file added

---
+++ practise/15.js
@@ -0,0 +1,14 @@
+let true_array = [true, , true, true, false, false, true];
+
+const countTrue = (check_array, check_value) => {
+  let new_array = check_array.filter((value) => {
+    if (check_value === value) {
+      return value;
+    }
+  });
+  return new_array.length;
+};
+
+const countTrue2 = (r) => r.filter((x) => x == true).length;
+console.log(countTrue2(true_array));
+// console.log(countTrue(true_array, true));
