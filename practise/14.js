faafa60828e3a0925b88786c469a972ff722dbb0
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-25 15:32:57 +0530

    1 file modified

--- practise/14.js
+++ practise/14.js
@@ -1,4 +1,4 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 9];
+// let array = [1, 2, 3, 4, 5, 6, 7, 9];
 // console.log(array.reverse());
 // array.forEach((index, value) => {
 //   console.log(index);
@@ -14,29 +14,38 @@ let array = [1, 2, 3, 4, 5, 6, 7, 9];
 // }, []);
 // console.log(new_array);
 
-let array2 = [
-  1,
-  7,
-  1,
-  2,
-  3,
-  5,
-  3,
-  'new',
-  'new2',
-  'new',
-  'new2',
-  3,
-  4,
-  5,
-  6,
-  7,
-  9,
-];
+// let array2 = [
+//   1,
+//   7,
+//   1,
+//   2,
+//   3,
+//   5,
+//   3,
+//   'new',
+//   'new2',
+//   'new',
+//   'new2',
+//   3,
+//   4,
+//   5,
+//   6,
+//   7,
+//   9,
+// ];
 
-let new_dupe = new Set([...array2]);
-let reverse = [...new_dupe].reduceRight((value, index) => {
-  value.push(index);
-  return value;
-}, []);
-console.log(reverse);
+// let new_dupe = new Set([...array2]);
+// let reverse = [...new_dupe].reduceRight((value, index) => {
+//   value.push(index);
+//   return value;
+// }, []);
+// console.log(reverse);
+
+// let new_array = [];
+// let reverse2 = array2.reduceRight((value, index) => {
+//   if (!new_array.includes(index)) {
+//     new_array.push(index);
+//   }
+//   return new_array;
+// }, []);
+// console.log(reverse2);
