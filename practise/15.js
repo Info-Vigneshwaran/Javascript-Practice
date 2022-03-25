10c3e1d991d5cb2971b269299d7416996540c2c7
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-25 17:02:22 +0530

    1 file modified

--- practise/15.js
+++ practise/15.js
@@ -1,4 +1,4 @@
-let true_array = [true, , true, true, false, false, true];
+let true_array = [true, true, true, false, false, true];
 
 const countTrue = (check_array, check_value) => {
   let new_array = check_array.filter((value) => {
@@ -12,3 +12,6 @@ const countTrue = (check_array, check_value) => {
 const countTrue2 = (r) => r.filter((x) => x == true).length;
 console.log(countTrue2(true_array));
 // console.log(countTrue(true_array, true));
+
+const leapYear = (year) => (year % 4 == 0 ? true : false);
+console.log(leapYear(2000));
