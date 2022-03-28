e9c0deec3a975bc280b0060652a5ac7061659d30
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-28 15:08:44 +0530

    1 file modified

--- practise/15.js
+++ practise/15.js
@@ -1,17 +1,27 @@
-let true_array = [true, true, true, false, false, true];
+// let true_array = [true, true, true, false, false, true];
 
-const countTrue = (check_array, check_value) => {
-  let new_array = check_array.filter((value) => {
-    if (check_value === value) {
-      return value;
-    }
-  });
-  return new_array.length;
-};
+// const countTrue = (check_array, check_value) => {
+//   let new_array = check_array.filter((value) => {
+//     if (check_value === value) {
+//       return value;
+//     }
+//   });
+//   return new_array.length;
+// };
 
-const countTrue2 = (r) => r.filter((x) => x == true).length;
-console.log(countTrue2(true_array));
+// const countTrue2 = (r) => r.filter((x) => x == true).length;
+// console.log(countTrue2(true_array));
 // console.log(countTrue(true_array, true));
 
-const leapYear = (year) => (year % 4 == 0 ? true : false);
-console.log(leapYear(2000));
+// const leapYear = (year) => (year % 4 == 0 ? true : false);
+// console.log(leapYear(2000));
+
+let neg_array = [10, -3, 7, -7, -9, 39];
+let pos_array = neg_array.filter((value, index) => {
+  return value > 0;
+});
+console.log(pos_array);
+let space_str = 'trr sfsdf sdaefsd wr 909 sdf0909';
+console.log(space_str.match(/\d+/g));
+console.log(space_str.match(/[aeiou]/g));
+console.log(space_str.replace(/\s/g, ''));
