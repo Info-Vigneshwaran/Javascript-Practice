901cc0c13510dcd7542e76e19309c20245bcb91c
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-08 16:19:46 +0530

    1 file modified

--- practise/16.js
+++ practise/16.js
@@ -9,14 +9,34 @@
 // const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 
 // console.log(fruits.myUcase());
-let check_array = [2, 77, 3, 5, 7, 97];
-let stack = 7;
-const SevenCheck = (check_array, stack) => {
-  return check_array.filter((value) => {
-    new_value = String(value).split('');
-    if (new_value.includes(String(stack))) {
-      return value;
-    }
-  });
-};
-console.log(SevenCheck(check_array, stack));
+let check_array = [1, 2, 3, 4, 5];
+// let stack = 7;
+// const SevenCheck = (check_array, stack) => {
+//   return check_array.filter((value) => {
+//     new_value = String(value).split('');
+//     if (new_value.includes(String(stack))) {
+//       return value;
+//     }
+//   });
+// };
+// console.log(SevenCheck(check_array, stack));
+let dup_arr = check_array.filter(
+  (item, index) => check_array.indexOf(item) !== index
+);
+let sum_arr = 0;
+let odd_arr = 0;
+let even_arr = 0;
+
+let ans_arr = check_array.map((item, value) => {
+  sum_arr += item;
+  if (item % 2 == 0) {
+    odd_arr += item;
+  } else {
+    even_arr += item;
+  }
+});
+console.log(sum_arr);
+console.log(odd_arr);
+console.log(even_arr);
+
+// console.log(dup_arr);
