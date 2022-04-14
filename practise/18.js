a7510499af37733a077727002b02ff3b44ac5849
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-14 17:27:20 +0530

    1 file modified

--- practise/18.js
+++ practise/18.js
@@ -12,10 +12,26 @@ Array.prototype.reshape = function (flate) {
 };
 
 let str = ['h', 'e', 'l'];
-console.log(str.join(''));
+// console.log(str.join(''));
 let x = [
   [1, 36, 5, 7],
   [10, 11, 16, 20],
   [23, 30, 34, 50],
 ];
-console.log(x.reshape(4));
+// console.log(x.reshape(4));
+
+var matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let new_matrix = matrix[0].map((val, index) => {
+  // console.log(index);
+  return matrix
+    .map((row) => {
+      return row[index];
+    })
+    .reverse();
+});
+console.log(new_matrix);
