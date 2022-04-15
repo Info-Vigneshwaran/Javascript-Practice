377c00a15e4f9787e672068e16f450b734ef987d
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-15 13:17:16 +0530

    1 file added

---
+++ practise/19.js
@@ -0,0 +1,14 @@
+// const set_Time = setTimeout(() => {
+//   console.log('sdfkj');
+// }, 0);
+// clearTimeout(set_Time);
+// const set_inter = setInterval(() => {
+//   console.log('Sf');
+// }, 1000);
+
+// clearInterval(set_inter);
+
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
