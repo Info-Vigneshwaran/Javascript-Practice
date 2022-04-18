e00e9dacbd4f334e2a1a63b56995d198ef2152f2
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-18 12:54:40 +0530

    1 file modified

--- practise/20.js
+++ practise/20.js
@@ -1,3 +1,11 @@
-let arr_text = [1, 7, 8, 9, 3];
-console.log(arr_text.indexOf(Math.max.apply('', arr_text)));
-console.log(arr_text.indexOf(Math.min.apply('', arr_text)));
+const x = () => {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+  let i = 10;
+};
+x();
+
+console.log(a);
+let a;
+a = 20;
