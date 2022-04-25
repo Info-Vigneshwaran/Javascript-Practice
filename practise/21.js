b15b460b53056e40626f9207169dd1be71a2cbf6
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-25 16:24:20 +0530

    1 file modified

--- practise/21.js
+++ practise/21.js
@@ -2,9 +2,13 @@ let a = [1, 2, 3, 8, 9];
 let b = [1, 2, 9, 10, 5];
 
 const new_arr = a.filter((prev, curr, self) => {
-  console.log(prev);
   if (b.indexOf(curr) == -1) {
     return curr;
   }
 });
-console.log(new_arr);
+
+let dup_arr = [2, 1, 1, 2, 3, 8, 9, 9, 5, 3];
+let uni_arr = dup_arr.filter((value, index, self) => {
+  return self.indexOf(value) == index;
+});
+console.log(uni_arr);
